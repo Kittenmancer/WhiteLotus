7bc79d8e02ac13a73a21e5f0e1608e6a89101a94
Kieran Bennett <kbennett@farnborough.ac.uk>
2015-12-02 18:31:39 +0000

    Entities/Users and Mappings/UsersMap mapped and finished.

--- WhiteLotus/Models/Entities/User.cs
+++ WhiteLotus/Models/Entities/User.cs
@@ -7,5 +7,29 @@ namespace WhiteLotus.Models.Entities
 {
     public class Users
     {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Forename { get; set; }
+        public virtual string Surname { get; set; }
+        public virtual DateTime DOB { get; set; }
+        public virtual int Experience { get; set; }
+        public virtual bool Health { get; set; }
+        public virtual int MOB { get; set; }
+        public virtual string Email { get; set; }
+        public virtual bool isDeveloper {get; set; }
+        public virtual bool isManager { get; set; }
+        public virtual string AddressRoad { get; set; }
+        public virtual string AddressTown { get; set; }
+        public virtual string AdressCounty { get; set; }
+        public virtual string AdressPostCode { get; set; }
+        public virtual string Password { get; set; }
+        public virtual bool Deleted { get; set; }
+
+
+        public virtual string UserType
+
+        {
+            get { return isDeveloper ? "Developer" : (isManager ? " Manager" : "Standard User"); }
+        }
     }
 }
---
+++ WhiteLotus/Models/Mappings/UsersMap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WhiteLotus.Models.Entities;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace WhiteLotus.Models.Mapping
+{
+    public class UsersMap:ClassMapping<Users>
+    {
+        public UsersMap()
+        {
+            Table("Users");
+
+            Id(x => x.Id, m => m.Generator(Generators.Identity));
+            Property(x => x.Title);
+            Property(x => x.Forename);
+            Property(x => x.Surname);
+            Property(x => x.DOB);
+            Property(x => x.Experience);
+            Property(x => x.Health);
+            Property(x => x.MOB);
+            Property(x => x.Email);
+            Property(x => x.AddressRoad);
+            Property(x => x.AddressTown);
+            Property(x => x.AdressCounty);
+            Property(x => x.AdressPostCode);
+            Property(x => x.Password);
+            Property(x => x.Deleted);
+            Property(x => x.isManager);
+            Property(x => x.isDeveloper);
+        }
+    }
+}
