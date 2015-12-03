fc0ec630f3adb6c2b63cac5bfa7efc622315810f
Chris Taylor <chris.taylor352@gmail.com>
2015-12-03 16:35:37 +0000

    -compelted Classes -added ClassesMap -added Booking -added BookingMap

---
+++ WhiteLotus/Models/Entities/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhiteLotus.Models.Entities
+{
+    public class Booking
+    {
+        public virtual int Id { get; set; }
+        public virtual int ClassBooked { get; set; }
+        public virtual int BookedBy { get; set; }
+        public virtual bool GonGiveItToYa { get; set; }
+    }
+}
---
+++ WhiteLotus/Models/Mappings/BookingMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WhiteLotus.Models.Entities;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+
+namespace WhiteLotus.Models.Mappings
+{
+    public class BookingMap:ClassMapping<Booking>
+    {
+        public BookingMap()
+        {
+            Table("Booking");
+
+            Id(x => x.Id, m => m.Generator(Generators.Identity));
+            Property(x => x.ClassBooked);
+            Property(x => x.BookedBy);
+            Property(x => x.GonGiveItToYa);
+        }
+    }
+}
