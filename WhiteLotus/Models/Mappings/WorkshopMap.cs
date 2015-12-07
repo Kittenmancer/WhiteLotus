c319c42d71cea9053829ce232f31d8ce664d3dac
Kittenmancer <chris.taylor352@gmail.com>
2015-12-07 19:18:25 +0000

    EVERYTHING IS KILL

---
+++ WhiteLotus/Models/Mappings/WorkshopMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WhiteLotus.Models.Entities;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace WhiteLotus.Models.Mappings
+{
+    public class WorkshopMap : ClassMapping<Workshop>
+    {
+        public WorkshopMap()
+        {
+            Table("Workshops");
+
+            Id(x => x.Id, m => m.Generator(Generators.Identity));
+            Property(x => x.Name);
+            Property(x => x.Date);
+            Property(x => x.StartTime);
+            Property(x => x.Duration);
+            Property(x => x.Capacity);
+            Property(x => x.TaughtBy);
+        }
+    }
+}
