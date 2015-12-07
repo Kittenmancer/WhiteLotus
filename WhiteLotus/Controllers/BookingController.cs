5e45a4b8efaaf0f79421794f2bb2f03294dec961
Kieran Bennett <kbennett@farnborough.ac.uk>
2015-12-07 23:37:39 +0000

    BookingController.

---
+++ WhiteLotus/Controllers/BookingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NHibernate;
+using WhiteLotus.Filters;
+using WhiteLotus.Models.Entities;
+using WhiteLotus.ViewModels;
+
+
+namespace WhiteLotus.Controllers
+{
+    public class BookingController : BaseController
+    {
+        public ActionResult Index()
+        {
+            var vM = new BookingViewModel();
+            return View(vM);
+        }
+    }
+}
