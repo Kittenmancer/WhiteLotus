9d6826d69b9c6d2ac8bf36efd98c67bd9e5ad649
Kieran Bennett <kbennett@farnborough.ac.uk>
2015-12-08 18:40:15 +0000

    Web.Config should be guuchi now.

--- WhiteLotus/ViewModels/BookingViewModel.cs
+++ WhiteLotus/ViewModels/BookingViewModel.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WhiteLotus.Models.Entities;
+using NHibernate;
 
 namespace WhiteLotus.ViewModels
 {
     public class BookingViewModel
     {
+        public Classes Class { get; set; }
+        public Workshop Workshop { get; set; }
     }
 }
